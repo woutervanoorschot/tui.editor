1bd29327fb4fdc374df11a33cb8dd4a03b4794b3
Wouter van Oorschot <woutervanoorschot@outlook.com>
2023-03-27 21:17:30 +0200

    Fix prosemirror type errors

--- apps/editor/types/editor.d.ts
+++ apps/editor/types/editor.d.ts
@@ -3,7 +3,7 @@ import { EditorView, Decoration, DecorationSet } from 'prosemirror-view';
 import { EditorState, Plugin, PluginKey, Selection, TextSelection } from 'prosemirror-state';
 import { undoInputRule, InputRule, inputRules } from 'prosemirror-inputrules';
 import { keymap } from 'prosemirror-keymap';
-import { Editor } from '@t/index';
+// import { Editor } from '@t/index';
 
 import {
   HTMLConvertor,
@@ -333,7 +333,7 @@ export interface Base {
 
   createSchema(): Schema;
 
-  createKeymaps(useCommandShortcut: boolean): Plugin<any, any>[];
+  createKeymaps(useCommandShortcut: boolean): Plugin<any>[];
 
   createCommands(): Record<string, EditorCommandFn<Record<string, any>>>;
 
